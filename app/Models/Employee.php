427f618ee2c67a58d665bb61696ebed6ed7db44b
Muhammad Haikal Syamsuddin <udingaming0903@gmail.com>
2025-11-09 04:18:05 +0700

    employee & department models

---
+++ app/Models/Employee.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nama',
+        'notelp',
+        'jabatan',
+        'departemen_id',
+        'status',
+    ];
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+}
